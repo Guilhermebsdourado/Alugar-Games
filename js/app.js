a91c6db7fe1bb2e8416a0792b68b6c4b277d43e1
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-05-31 19:16:44 -0300

    Alterando Statusdo Jogo Clicado

--- js/app.js
+++ js/app.js
@@ -4,15 +4,21 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        let resposta = confirm ('Você tem certeza que quer devolver?');
+
+        if (resposta) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
-        
-    } else {
+        } else {
+        alert('Você canselou a devolução');
+        }  
+    } else {  
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
     }
+    contarJogosAlugados();
 }
 
 
